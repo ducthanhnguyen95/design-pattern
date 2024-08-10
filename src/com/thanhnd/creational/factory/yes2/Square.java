84e95291fd7ac9ba52ece751bfd56e3c05abcd64
thanhnd <ducthanhnguyen95@gmail.com>
2024-08-10 14:53:40 +0700

    Factory method pattern

---
+++ src/com/thanhnd/creational/factory/yes2/Square.java
@@ -0,0 +1,8 @@
+package com.thanhnd.creational.factory.yes2;
+
+public class Square implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Drawing a square");
+    }
+}
---
+++ src/com/thanhnd/creational/factory/yes2/SquareFactory.java
@@ -0,0 +1,8 @@
+package com.thanhnd.creational.factory.yes2;
+
+public class SquareFactory extends ShapeFactory{
+    @Override
+    public Shape createShape() {
+        return new Square();
+    }
+}
